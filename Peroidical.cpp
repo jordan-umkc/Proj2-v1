c9927d1e4e7f370315866de0b78da6a7b1d6b369
evanbell123 <evanbell123@yahoo.com>
2014-07-12 15:18:43 -0500

    gfd

--- Peroidical.cpp
+++
@@ -1,8 +0,0 @@
-#include "Periodical.h"
-
-void Periodical::generateEmpQueue(map<string,Employee>& empMap)
-{
-	for (map<string, Employee>::iterator itr = empMap.begin(); itr != empMap.end(); itr++){
-		empQueue.push(itr->second);
-	}
-}
